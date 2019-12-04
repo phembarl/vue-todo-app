737caafc926695d109fe54348a90708de385bbbe
Femi Balogun <phembarl@yahoo.com>
2019-12-04 13:59:53 +0100

    initial commit

---
+++ src/views/About.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>Waddup y'all! This is the TodoApp v1 baby!</p>
+  </div>
+</template>
+
+<style scoped>
+.about {
+  margin: 10px;
+}
+</style>
